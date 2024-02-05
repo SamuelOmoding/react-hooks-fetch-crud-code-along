16ab98a3abd2cfc3dd9cd94cb2c0aa4eb9a38fcb
Omoding-web <samuel.omoding@student.moringaschool.com>
2024-02-05 13:46:16 +0300

    Assignment done

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,12 @@ import Header from "./Header";
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const [items, setItems] = useState([]);
+
+  const handleDelete = (itemId) => {
+    const updateItems = items.filter(item => item.id !== itemId);
+    setItems(updateItems);
+  };
 
   function handleDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
@@ -13,6 +19,12 @@ function App() {
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
       <ShoppingList />
+      <h1>Items</h1>
+      <ul>
+        {items.map(item => (
+          <item key={item.id} item={item} onDelete={handleDelete} />
+        ))}
+      </ul>
     </div>
   );
 }
--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,11 +1,42 @@
 import React, { useState } from "react";
 
-function ItemForm() {
+function ItemForm({ onAddItem }) {
   const [name, setName] = useState("");
   const [category, setCategory] = useState("Produce");
 
+  // Add function to handle submissions
+  function handleSubmit(e) {
+    e.preventDefault();
+    const itemData = {
+      name: name,
+      category: category,
+      isInCart: false,
+    };
+
+    fetch("http://localhost:4000/items", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(itemData),
+    })
+      .then((r) => {
+        if (!r.ok) {
+          throw new Error("Failed to add item");
+        }
+        return r.json();
+      })
+      .then((newItem) => {
+        onAddItem(newItem);
+        setName(""); // Clear the input after submission
+        setCategory("Produce"); // Reset category to default after submission
+      })
+      .catch((error) => console.error("Error adding item:", error));
+  }
+
   return (
-    <form className="NewItem">
+    // Set up the form to call handleSubmit when the form is submitted
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
         <input
